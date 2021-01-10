60bc88d308b0299974d617cbc0c35d3b4a7adb36
Gamer9311 <77152040+Gamer9311@users.noreply.github.com>
2021-01-10 11:22:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function add() {
+    var result = Number(document.getElementById("num1").value) + Number(document.getElementById("num2").value);
+    window.alert(result);
+}
+
+function sub() {
+    var result = Number(document.getElementById("num1").value) - Number(document.getElementById("num2").value);
+    window.alert(result);
+}
+
+function multi() {
+    var result = Number(document.getElementById("num1").value) * Number(document.getElementById("num2").value);
+    window.alert(result);
+}
+
+function div() {
+    var result = Number(document.getElementById("num1").value) / Number(document.getElementById("num2").value);
+    window.alert(result);
+}
